dfb1543cbba4771bb18c53a69815f48bc2180586
65656508 <aktailakov.n.81@gmail.com>
2026-06-29 16:35:00 +0500

    Add data files and global styles

---
+++ data/links.ts
@@ -0,0 +1,36 @@
+// Color palette
+export const COLORS = {
+  darkBg: '#0C1410',
+  darkGreen: '#1B4D3E',
+  gold: '#C9A84C',
+  lightGray: '#E8E8E8',
+  white: '#FFFFFF',
+};
+
+// Logo
+export const LOGO_URL = 'https://media.base44.com/images/public/user_6a3a7160817fddb597912387/d32e73f16_IMG_20260624_000919.png';
+
+// External links
+export const LINKS = {
+  whatsapp: 'https://wa.me/message/JY446DE2EZQRM1',
+  instagram: 'https://www.instagram.com/newcastle.barbershop',
+  twoGis: 'https://2gis.kz/kyzylorda/geo/70000001082411408',
+  phone: '+7 700 344 4682',
+};
+
+// Hero image
+export const HERO_IMAGE = 'https://media.base44.com/images/public/user_6a3a7160817fddb597912387/f90d91b5f_IMG-20260620-WA0037.jpg';
+
+// Training image
+export const TRAINING_IMAGE = 'https://media.base44.com/images/public/user_6a3a7160817fddb597912387/95e28eace_IMG_20260624_014441.png';
+
+// Coffee service image
+export const COFFEE_IMAGE = 'https://media.base44.com/images/public/user_6a3a7160817fddb597912387/b0b182e97_Screenshot_2026-06-23-23-09-27-386_cominstagramandroid.png';
+
+// Gallery images
+export const GALLERY_IMAGES = [
+  'https://media.base44.com/images/public/user_6a3a7160817fddb597912387/d33b1e81b_IMG_20260620_233254.png',
+  'https://media.base44.com/images/public/user_6a3a7160817fddb597912387/1cc3b62eb_IMG_20260620_233355.png',
+  'https://media.base44.com/images/public/user_6a3a7160817fddb597912387/578c3357e_IMG_20260620_233602.jpg',
+  'https://media.base44.com/images/public/user_6a3a7160817fddb597912387/578c3357e_IMG_20260620_233602.jpg',
+];
